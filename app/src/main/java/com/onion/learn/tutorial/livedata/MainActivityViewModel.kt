962b4288a7c5b6262cdcad8f71e5607ffa4d269e
skullzero <skullzero@163.com>
2021-10-02 00:38:07 +0800

    add ViewModel

---
+++ app/src/main/java/com/onion/learn/tutorial/livedata/MainActivityViewModel.kt
@@ -0,0 +1,41 @@
+package com.onion.learn.tutorial.livedata
+
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+    private lateinit var timer: CountDownTimer
+
+    /*
+        for every LiveData object, should have privat MutableLiveData, and public LiveData which will
+        be exposed to other app components
+    */
+    private val _seconds = MutableLiveData<Int>()
+    val seconds: LiveData<Int>
+        get() = _seconds
+
+    private val _finished = MutableLiveData<Boolean>()
+    val finished: LiveData<Boolean>
+        get() = _finished
+
+    var timerValue = MutableLiveData<Long>()
+
+    fun startTimer() {
+        timer = object: CountDownTimer(timerValue.value!!, 1000) {
+            override fun onFinish() {
+                _finished.value = true
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                _seconds.value = timeLeft.toInt()
+            }
+        }.start()
+    }
+
+    fun stopTimer(){
+        timer.cancel()
+    }
+}
